7833f6d9e547f6536bb34f1da7d21644a8b8af15
Lucas Gonzalez <lucasggonzalez94@outlook.com>
2024-09-09 20:40:40 -0300

    feat: configuracion fuentes y husky

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { rootFont } from "./config/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${rootFont.className} antialiased`}
       >
         {children}
       </body>
