7e8d71eef058f1f3b7ccb1eaeca9e19e912c4dd5
Lucas Gonzalez <lucasggonzalez94@outlook.com>
2024-10-02 21:23:47 -0300

    feat: config nextui

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,7 +1,16 @@
+import { Button } from '@nextui-org/react';
+
 export default function HomeLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <main className="min-h-screen bg-red-500">{children}</main>;
+  return (
+    <main className="min-h-screen bg-zinc-200">
+      <Button color="primary" variant="flat" radius="md">
+        Explorar Atracciones
+      </Button>
+      {children}
+    </main>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from 'tailwindcss';
+import { nextui } from '@nextui-org/react';
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
@@ -14,6 +16,33 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    nextui({
+      layout: {
+        disabledOpacity: '0.3', // opacity-[0.3]
+        radius: {
+          small: '2px', // rounded-small
+          medium: '4px', // rounded-medium
+          large: '6px', // rounded-large
+        },
+        borderWidth: {
+          small: '1px', // border-small
+          medium: '1px', // border-medium
+          large: '2px', // border-large
+        },
+      },
+      themes: {
+        light: {
+          colors: {
+            primary: {
+              DEFAULT: '#E95718',
+              foreground: '#ffffff',
+            },
+            focus: '#E95718',
+          },
+        },
+      },
+    }),
+  ],
 };
 export default config;
