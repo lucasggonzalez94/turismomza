c23e1a159deae38759bde12d451986d1cdd4aa5d
Lucas Gonzalez <lucasggonzalez94@outlook.com>
2024-10-05 20:35:30 -0300

    feat: responsive cards

--- src/components/home/sections/AttranctionsHome.tsx
+++ src/components/home/sections/AttranctionsHome.tsx
@@ -1,11 +1,13 @@
 import AttractionCard from '@/components/ui/AttractionCard/AttractionCard';
 import { Attraction } from '@/interfaces/attraction';
 import { getAttractions } from '@/services/attractions/get-attractions';
+import { Button } from '@nextui-org/react';
+import Link from 'next/link';
 
 const AttractionsHome = async () => {
   const { data: attractions } = await getAttractions();
   return (
-    <div className="flex flex-col gap-4 h-screen p-12">
+    <div className="flex flex-col gap-4 h-auto p-12">
       <h2 className="font-bold text-3xl">Atractivos</h2>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 grid-rows-2 gap-4">
         <>
@@ -13,6 +15,11 @@ const AttractionsHome = async () => {
             <AttractionCard key={attraction?.id} attraction={attraction} />
           ))}
         </>
+      </div>
+      <div className="w-full flex justify-center items-center mt-5">
+        <Button color="primary" as={Link} href="/attractions">
+          Ver todos
+        </Button>
       </div>
     </div>
   );
