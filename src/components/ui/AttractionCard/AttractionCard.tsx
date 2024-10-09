f7b29162c511149748f2da8b3247edbedd65f583
Lucas Gonzalez <lucasggonzalez94@outlook.com>
2024-10-09 20:18:16 -0300

    feat: cards responsive

--- src/components/ui/AttractionCard/AttractionCard.tsx
+++ src/components/ui/AttractionCard/AttractionCard.tsx
@@ -44,10 +44,10 @@ const AttractionCard: FC<IPropsAttractionCard> = ({ attraction }) => {
 
   return (
     <div
-      className={`flex flex-col rounded-md w-[100%] bg-white overflow-hidden border cursor-pointer transition-transform duration-300 transform hover:scale-105 ${advertisements?.length ? 'border-yellow-500 border-2' : 'border-gray-300'}`}
+      className={`flex h-32 sm:h-auto sm:flex-col rounded-md w-[100%] bg-white overflow-hidden border cursor-pointer transition-transform duration-300 transform hover:scale-105 ${advertisements?.length ? 'border-yellow-500 border-2' : 'border-gray-300'}`}
       onClick={() => handleNavigation(`/${slug}`)}
     >
-      <div className="relative">
+      <div className="relative flex justify-center items-center min-w-40">
         {advertisements?.length ? (
           <div className="absolute left-2 top-2 flex items-center gap-1 bg-yellow-500 rounded-sm p-1">
             <IoStar size={12} className="mb-[2px]" />
@@ -71,7 +71,7 @@ const AttractionCard: FC<IPropsAttractionCard> = ({ attraction }) => {
             alt={`Foto ${title}`}
             width={300}
             height={200}
-            className="object-cover w-full h-44"
+            className="object-cover w-full h-full"
             // TODO: Modificar si se carga
             priority
           />
@@ -81,28 +81,45 @@ const AttractionCard: FC<IPropsAttractionCard> = ({ attraction }) => {
             alt="No image"
             width={300}
             height={200}
-            className="object-cover w-full h-44"
+            className="object-cover w-full h-full"
             priority
           />
         )}
       </div>
-      <div className="flex flex-col gap-2 p-3">
-        <div className="flex flex-col">
-          <h3 className="font-bold line-clamp-1">{title}</h3>
+
+      <div className="flex items-center sm:items-start justify-between sm:flex-col sm:gap-2 p-3 w-full">
+        <div className="flex flex-col w-full grow">
+          <div className="flex justify-between items-center gap-4">
+            <h3 className="font-bold line-clamp-1 text-lg md:text-sm lg:text-md xl:text-lg">
+              {title}
+            </h3>
+            <span className="font-bold hidden sm:inline-block text-gray-500">
+              1.3Km
+            </span>
+          </div>
           <span className="text-tiny text-gray-400">{category}</span>
         </div>
-        <hr />
-        <div className="flex justify-between items-center">
-          <Button color="primary" onClick={() => handleNavigation(`/${slug}`)}>
+
+        <hr className="hidden w-full sm:inline-block" />
+
+        <div className="flex justify-between items-center h-full min-w-28 sm:w-full">
+          <Button
+            color="primary"
+            onClick={() => handleNavigation(`/${slug}`)}
+            className="hidden sm:block"
+          >
             Ver más
           </Button>
-          <div className="flex flex-col text-end">
-            <span className="font-bold text-[#E95718] flex justify-end items-center gap-1">
-              <IoStar className="mb-[2px]" /> {averageRating}
-            </span>
-            <span className="text-tiny text-gray-400">
-              {`(${comments?.length} opiniones)`}
-            </span>
+          <div className="flex flex-col justify-between items-end h-full w-full">
+            <span className="font-bold sm:hidden text-gray-500">1.3Km</span>
+            <div className="flex flex-col text-end">
+              <span className="font-bold text-[#E95718] flex justify-end items-center gap-1">
+                <IoStar className="mb-[2px]" /> {averageRating}
+              </span>
+              <span className="text-tiny text-gray-400">
+                {`(${comments?.length} opiniones)`}
+              </span>
+            </div>
           </div>
         </div>
       </div>
