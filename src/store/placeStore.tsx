3b50425947adb8a5bda1ae74a8498172996a6095
Lucas Gonzalez <lucasggonzalez94@outlook.com>
2025-04-15 23:31:52 -0300

    refactor: store sin usar

--- src/store/placeStore.tsx
+++ src/store/placeStore.tsx
@@ -1,17 +1,7 @@
 import { IPlaceFormContact, IPlaceFormDetails } from '@/interfaces/place-form';
-import { Filters } from '@/interfaces/filters';
 import { create } from 'zustand';
 
-interface Price {
-  minPrice: number;
-  maxPrice: number;
-}
-
 interface State {
-  filters: Filters | null;
-  setFilters: (filters: Filters | null) => void;
-  prices: Price;
-  setPrices: (prices: Price) => void;
   placeFormDetails: IPlaceFormDetails | null;
   setPlaceFormDetails: (data: IPlaceFormDetails | null) => void;
   placeFormContact: IPlaceFormContact | null;
@@ -19,13 +9,6 @@ interface State {
 }
 
 export const usePlaceStore = create<State>((set) => ({
-  filters: null,
-  setFilters: (filters) => set(() => ({ filters })),
-  prices: {
-    minPrice: 0,
-    maxPrice: 0,
-  },
-  setPrices: (prices) => set(() => ({ prices })),
   placeFormDetails: null,
   setPlaceFormDetails: (data) => set(() => ({ placeFormDetails: data })),
   placeFormContact: null,
